e7aeada6e1f4ae77c9e0d17ff2f6e6a6a8f02029
Tom LaMantia <tom@lamantia.org>
2018-09-08 14:29:45 -0400

    BUG: Only mark vertex once at each call and return dfs order.

--- src/dfsPaths.scala
+++ src/dfsPaths.scala
@@ -7,20 +7,17 @@ class dfsPaths {
   var markedVertices: ArrayBuffer[Int] = null
 
   private def dfs(g: Graph, v: Int): Unit ={
-
     this.markedVertices += v
-
     for (u <- g.adj(v)){
       if (!this.markedVertices.contains(u)){
-        this.markedVertices += u
-        println(u)
         this.dfs(g, u)
       }
     }
   }
 
-  def dfsClient(g: Graph, v : Int): Unit = {
+  def dfsClient(g: Graph, v : Int): ArrayBuffer[Int] ={
     this.markedVertices = new ArrayBuffer[Int]()
     this.dfs(g, v)
+    this.markedVertices
   }
 }
